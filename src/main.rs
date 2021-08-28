59a3409cb8a62e5242e21ee14cda4c75fdcafb5c
Ryu18 <ryu@tpgjbo.xyz>
2021-08-28 17:50:54 +0200

    added logger content-type and cache-control

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
-use actix_web::{client, get, web, App, HttpResponse, HttpServer};
+use actix_web::{client, get, web, middleware, App, HttpResponse, HttpServer};
 use serde::Deserialize;
 use std::io::Cursor;
+use env_logger::Env;
 
 #[derive(Deserialize)]
 pub struct QueryParameters {
@@ -68,13 +69,19 @@ async fn bypass_clyde(web::Query(info): web::Query<QueryParameters>) -> HttpResp
     let gif = create_gif(image)
         .expect("Gif Creation Error");
     // println!("create_gif success");
-    HttpResponse::Ok().body(gif)
+    HttpResponse::Ok()
+        .header("Content-Type", "image/gif")
+        .header("Cache-Control", "max-age=31536000")
+        .body(gif)
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
     HttpServer::new(|| {
         App::new()
+            .wrap(middleware::Logger::default())
+            .wrap(middleware::Logger::new("%a %{User-Agent}i"))
             .service(bypass_clyde)
     })
     .bind("127.0.0.1:23423")?
